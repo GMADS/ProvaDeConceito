da0b7a680e5a0109c0d4509b2d326e11518155a2
Guilherme Miguel <guilherme.souza1996@hotmail.com>
2023-06-06 21:50:19 -0300

    Testando funcionamento NBomber

---
+++ NBomber/src/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+using NBomber.Plugins.Network.Ping;
+
+namespace NBomberTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var step = Step.Create("fetch_html_page", 
+                                   clientFactory: HttpClientFactory.Create(), 
+                                   execute: context =>
+            {
+                var request = Http.CreateRequest("GET", "https://nbomber.com")
+                                  .WithHeader("Accept", "text/html");
+
+                return Http.Send(request, context);
+            });
+
+            var scenario = ScenarioBuilder
+                .CreateScenario("simple_http", step)
+                .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+                .WithLoadSimulations(
+                    Simulation.InjectPerSec(rate: 100, during: TimeSpan.FromSeconds(30))
+                );
+
+            // creates ping plugin that brings additional reporting data
+            var pingPluginConfig = PingPluginConfig.CreateDefault(new[] {"nbomber.com"});
+            var pingPlugin = new PingPlugin(pingPluginConfig);                
+
+            NBomberRunner
+                .RegisterScenarios(scenario)
+                .WithWorkerPlugins(pingPlugin)
+                .Run();
+
+        }
+    }
+}
